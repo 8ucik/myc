6eb9d038fcfd1e58edcc283939535e59dfd93a6f
Tomasz Urbaniak <tomekgnu@gmail.com>
2020-02-20 21:43:04 +0100

    Run two timers at once

--- small_programs/timers/main.c
+++ small_programs/timers/main.c
@@ -12,10 +12,26 @@
 #include <string.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <pthread.h>
 
 void notify(sigval_t sig);
+int makeTimer(int t);
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 int main(void){
+
+	makeTimer(3);
+	makeTimer(4);
+
+	while(1){
+		sleep(3600);
+	}
+
+	return 0;
+}
+
+
+int makeTimer(int t){
 	struct sigevent se = {0};
 	struct itimerspec its = {0};
 	timer_t timer_id = 0;
@@ -25,25 +41,22 @@ int main(void){
 	se.sigev_notify = SIGEV_THREAD;
 	se.sigev_value.sival_ptr = &timer_id;
 	se.sigev_notify_attributes = NULL;
+	se.sigev_value.sival_int = t;
 	status = timer_create(CLOCK_REALTIME,&se,&timer_id);
 
 	printf("status: %d\ntimerid: %p\nerr: %d\ndesc: %s\n",status,timer_id,errno,strerror(errno));
 
-	its.it_interval.tv_sec = 1;
+	its.it_interval.tv_sec = t;
 	its.it_interval.tv_nsec = 0;
-	its.it_value.tv_sec = 1;
+	its.it_value.tv_sec = t;
 	its.it_value.tv_nsec = 0;
 	timer_settime(timer_id, 0, &its, NULL);
-
-	while(1){
-
-	}
 
 	return 0;
 }
 
 void notify(sigval_t sig){
-
-	printf("Timer pid: %d\n",getpid());
-
+	pthread_mutex_lock (&mutex);
+	printf("Timer sigval: %d\n",sig.sival_int);
+	pthread_mutex_unlock (&mutex);
 }
